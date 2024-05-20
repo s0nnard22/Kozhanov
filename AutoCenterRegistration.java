851d240e54326e79913757d74225f7dee8b0eb8b
s0nnard22 <152876677+s0nnard22@users.noreply.github.com>
2024-05-20 15:53:45 +0700

    Update AutoCenterRegistration.java

--- AutoCenterRegistration.java
+++ AutoCenterRegistration.java
@@ -11,7 +11,7 @@ public class AutoCenterRegistration extends JFrame {
     private JButton btnRegister;
 
     public AutoCenterRegistration() {
-        // Инициализация компонентов интерфейса
+        
         createGUI();
     }
 
@@ -21,16 +21,14 @@ public class AutoCenterRegistration extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
-        // Создание текстовых полей
+      
         txtClientName = new JTextField(15);
         txtCarModel = new JTextField(15);
         txtAreaResults = new JTextArea(5, 20);
         txtAreaResults.setEditable(false);
 
-        // Создание кнопок
         btnRegister = new JButton("Регистрация");
 
-        // Добавление компонентов на форму
         add(new JLabel("ФИО Клиента:"));
         add(txtClientName);
         add(new JLabel("Модель Авто:"));
@@ -38,7 +36,7 @@ public class AutoCenterRegistration extends JFrame {
         add(btnRegister);
         add(new JScrollPane(txtAreaResults));
 
-        // Добавление слушателя на кнопку регистрации
+   
         btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -46,20 +44,17 @@ public class AutoCenterRegistration extends JFrame {
             }
         });
 
-        // Отображение окна
+    
         setVisible(true);
     }
 
-    // Метод для обработки нажатия кнопки регистрации
     private void registerClient() {
         String clientName = txtClientName.getText();
         String carModel = txtCarModel.getText();
 
-        // Здесь должна быть логика регистрации клиента и авто...
-        // Для примера мы просто выводим информацию в текстовую область
         txtAreaResults.append("Клиент " + clientName + " с авто " + carModel + " зарегистрирован.\n");
 
-        // Очистка текстовых полей после регистрации
+   
         txtClientName.setText("");
         txtCarModel.setText("");
     }
